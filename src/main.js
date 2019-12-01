1a1d4a0eb1e286caa228f17f953336cc568ac050
sasinskil <sasinskil@gmail.com>
2019-12-01 15:51:28 +0100

    add vue-resource, vue-router, font awesome config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+library.add(faMoon);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   render: h => h(App)
